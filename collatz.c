02ce137007234851cb05e780edc4537cce18c549
Gabriel Passos <gabriel80546@gmail.com>
2024-05-04 03:31:55 -0300

    ./push.sh

---
+++ collatz.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int max = 1000000;
+int inicial = 1;
+
+void collatz(int x) {
+    inicial++;
+    if(inicial >= max) {
+        exit(1);
+    }
+	printf("%d ", x);
+	if (x <= 1)
+		return;
+	else if (x % 2 == 0)
+		collatz(x/2);
+	else
+		collatz(3*x+1);
+}
+
+
+int main() {
+    printf("Collatz Conjecture\n");
+    int i;
+
+    for (i = 1; i < 1000000; i++) {
+        printf("collatz(%i) = ", i);
+        collatz(i);
+        printf("\n");
+    }
+
+    /*
+    printf("\n\n.\n\n");
+    for (i = 0; i < 100; i++) {
+        printf("collatz(%i) = ", -i);
+        collatz(-i);
+        printf("\n");
+    }
+    */
+    return 0;
+}
